a1ad02e31d929d29e78225f277d667108bc1451c
Jose Olinda <joseolindad@yahoo.com.br>
2022-02-17 18:18:27 -0300

    Avaliação

--- js/poke.js
+++ js/poke.js
@@ -2,24 +2,27 @@ const createCard = (pokemon) => {
     const cards = document.querySelector(".container .cards");
 
     let tipos = "";
+    let tipo = "";
+    let tipo_base = pokemon.types[0].type.name;
 
     for(let i = 0; i < pokemon.types.length; i++) {
-        tipos += `<li>${pokemon.types[i].type.name}</li>`;
+        tipo = pokemon.types[i].type.name;
+        tipos += `<li class="${tipo}">${tipo}</li>`;
     }
 
     cards.innerHTML += `
-        <div class="card">
-            <span class="name">#${pokemon.id}</span>
-            <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="" class="photo">
-            <span class="name">${pokemon.name}</span>
-            <span class="forms">Tipo: </span>
-            <ul class="list_forms">
+        <div class="card ${tipo_base}">
+            <span class="id ${tipo_base}">#${pokemon.id}</span>
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="" class="photo">
+            <span class="name ${tipo_base}">${pokemon.name}</span>
+            <ul class="list_types">
                 ${tipos}
             </ul>
         </div>
     `;
 }
 const getPokemon = pokeID => {
+    const limit = 600;
     const ajax = new XMLHttpRequest();
 
     ajax.open("get", "https://pokeapi.co/api/v2/pokemon/" + pokeID);
@@ -27,12 +30,15 @@ const getPokemon = pokeID => {
 
     ajax.onreadystatechange = function() {
         if( this.readyState == 4 && this.status == 200) {
+            pokemonCarregado = true;
             let pokemon = JSON.parse(this.responseText);
             createCard(pokemon);
+            if (pokeID < limit) {
+                pokeID++;
+                getPokemon(pokeID);
+            }
         }
     }
 }
 
-for(let i = 1; i <= 151; i++) {
-    getPokemon(i);
-}
+getPokemon(1);
